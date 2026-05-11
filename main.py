7b53d85be559dbfe07d3642a143e7f9240af6b30
anuj <anujtiwari000@outlook.com>
2026-05-11 17:12:00 +0530

    frist commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import cv2
+import mediapipe as mp
+
+cap = cv2.VideoCapture(0)
+
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands()
+
+mp_draw = mp.solutions.drawing_utils
+
+while True:
+    success, img = cap.read()
+
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    results = hands.process(imgRGB)
+
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            mp_draw.draw_landmarks(
+                img,
+                handLms,
+                mp_hands.HAND_CONNECTIONS
+            )
+
+    cv2.imshow("Hand Tracking", img)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
